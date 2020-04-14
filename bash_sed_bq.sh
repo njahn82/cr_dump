c4b58621280767082520c53afc753a9213e6d08c
Najko Jahn <najko.jahn@gmail.com>
2020-04-14 12:40:22 +0200

    make metadata fields compliant with bq,i.e. avoid . in json field names

---
+++ bash_sed_bq.sh
@@ -0,0 +1,9 @@
+FILES=~/Documents/r-projects/cr_dump/data_parsed/data_parsed/*
+for i in $FILES; do
+    sed -i '' 's/container.title/container_title/g' $i
+    sed -i '' 's/delay.in.days/delay_in_days/g' $i
+    sed -i '' 's/content.version/content_version/g' $i
+    sed -i '' 's/content.type/content_type/g' $i
+    sed -i '' 's/intended.application/intended_application/g' $i
+    echo "Processing $i"
+done
