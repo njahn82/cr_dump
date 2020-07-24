c55528a32c4489e4b93b00cfe28195005a1dfbf7
Ahobert <anne@hobert.eu>
2020-07-24 14:44:48 +0200

    added author_count, fixed has_abstract and renaming of nested columns

--- cr_dump_parser.R
+++ cr_dump_parser.R
@@ -16,9 +16,15 @@ cr_parse <- function(in_file, out_dir) {
       # only journal articles
       filter(type == "journal-article") %>%
       # abstract present?
-      mutate(has_abstract = is.na(abstract)) %>%
+      mutate(has_abstract = ifelse("abstract" %in% names(.),
+                                   ifelse(!is.na(abstract), TRUE, FALSE),
+                                   FALSE)) %>%
+      # authorcount
+      mutate_at(vars(one_of(c("author"))),
+                .funs = list(author_count = function(x) map(x, count_authors))) %>%
       # only relevant fields
-      select(one_of(cr_md_fields), has_abstract) %>%
+      select(one_of(cr_md_fields), has_abstract, author_count) %>%
+      # date parsing
       mutate(created = lubridate::parse_date_time(created, c("y", "ymd", "ym"))) %>%
       mutate_at(vars(one_of(
         c("published.print", "published.online")
@@ -27,10 +33,9 @@ cr_parse <- function(in_file, out_dir) {
       mutate(issued = lubridate::parse_date_time(issued, c("y", "ymd", "ym"))) %>%
       mutate(issued_year = lubridate::year(issued)) %>%
       filter(issued_year > 2007) %>%
+      # remove "." from colnames and nested colnames
       rename_at(vars(contains(".")), function(x) gsub("\\.", "_", x)) %>%
-      mutate_at(vars(one_of(c("license", "link"))),
-                 function(z) map(z, ~ .x %>%
-                             rename_at(vars(contains(".")), function(y) gsub("\\.", "_", y)))) %>%
+      mutate_at(vars(one_of(c("license", "link"))), function(x) map(x, rename_nested_columns)) %>%
       mutate(file_name = in_file)
     if (!nrow(out) == 0) {
       out_file <- gsub("data", out_dir, in_file)
@@ -69,4 +74,21 @@ cr_test <- function(req) {
   ), make_date), c("y", "ymd", "ym")))
   types <- sapply(req[["items"]], "[[", "type")
   list(years = years, types = types)
+}
+
+rename_nested_columns <- function(x) {
+  if(!is.na(x)){
+    x <- as.data.frame(x) %>% rename_at(vars(contains(".")),
+                                        function(y) gsub("\\.", "_", y))
+  }
+  return(x)
+}
+
+count_authors <- function(x){
+  if(is.na(x)) {
+    cnt <- 0
+  } else {
+    cnt <- as.data.frame(x) %>% nrow()
+  }
+  return(cnt)
 }
